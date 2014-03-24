e0c58ff614a3543ca0877e303ccec1ee3aaf2965
dtredger <d.tredger@gmail.com>
2014-03-24 10:31:57 -0400

    smoothed head motion

--- robot_servo_light_sensor/robot_servo_light_sensor.ino
+++ robot_servo_light_sensor/robot_servo_light_sensor.ino
@@ -9,20 +9,35 @@ int servoVal;
 int lightsensorPin = 0;
 int lightValue;
 
+const int numReadings = 10;
+int readings[numReadings];
+int index = 0;
+int total = 0;
+int rollingAverage = 0; 
 
 void setup(){
   Serial.begin(9600);
   robotServo.attach(servoPin);
+
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+    readings[thisReading] = 0; 
 }
 
 void loop(){
-//The light-sensor is around 150 in normal indoor light; 
-//at 5 directly under bulb, 600 totally obscured
-  lightValue = analogRead(lightsensorPin);
-  servoVal = map(lightValue, 10, 500, 0, 179);
-  Serial.println(lightValue);
-  Serial.println(servoVal);
+
+  total = total - readings[index];         
+  readings[index] = analogRead(lightsensorPin); 
+  total = total + readings[index];       
+  index = index + 1;                    
+
+  if (index >= numReadings)              
+    index = 0;                           
+  rollingAverage = total / numReadings;          
+  delay(10); 
+
+  //The light-sensor is around 150 in normal indoor light; 
+  //at 5 directly under bulb, 600 totally obscured
+  servoVal = map(rollingAverage, 10, 500, 0, 179);
   robotServo.write(servoVal);
-  delay(15);
 }
 
